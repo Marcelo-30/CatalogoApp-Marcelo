57fd3da198523e9fb015da99726334e6353cc4fe
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-06-24 12:27:00 -0600

    Limpiar archivos innecesarios del repositorio

--- CatalogoRopaMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CatalogoRopaMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- CatalogoRopaMVC/wwwroot/js/site.js
+++
@@ -1,1 +0,0 @@
-console.log("Catálogo de Ropa MVC cargado correctamente.");
